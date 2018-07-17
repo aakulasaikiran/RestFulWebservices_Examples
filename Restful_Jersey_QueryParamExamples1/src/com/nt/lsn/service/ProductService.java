f76394fa49707646daac3bf90f332e95fafe55ad
saikiran <aakulasaikiran>
2018-07-17 13:12:12 +0530

    commit1

---
+++ Restful_Jersey_QueryParamExamples1/src/com/nt/lsn/service/ProductService.java
@@ -0,0 +1,137 @@
+package com.nt.lsn.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.nt.lsn.domain.Product;
+import com.sun.media.jfxmedia.Media;
+
+@Path("ProductService")
+public class ProductService {
+
+	
+/*
+	 1 XML OUT PUT
+	 
+	 Input 
+	 http://localhost:8080/Restful_Jersey_QueryParamExamples/ProductService/getProductinXML?pid=301
+	 
+	 
+	 OutPut
+	 <?xml version="1.0" encoding="UTF-8" standalone="true"?>
+
+-<product>
+
+<pid>301</pid>
+
+<pname>computer</pname>
+
+<price>300.0</price>
+
+</product>
+	*/
+	
+	/*
+	@Path("/getProductinXML")
+	@Produces(MediaType.APPLICATION_XML)
+	
+	@GET
+	public Product searchProduct1(@QueryParam("pid") Integer pid) {
+		System.out.println("entered in searchProduct1 Method");
+		Product p = null;
+		if (pid != null && pid.equals(301)) {
+			p = new Product();
+			p.setPid(pid);
+			p.setPname("computer");
+			p.setPrice(300.0);
+		}
+
+		return p;
+
+	}
+
+	*/
+	
+	
+	/*
+	 2  for JSON
+	 
+	 
+	 Input
+	 http://localhost:8080/Restful_Jersey_QueryParamExamples/ProductService/getProductinJSON?pid=301 
+	  
+	  Out Put
+	  {
+   "pid": 301,
+   "pname": "computer",
+   "price": 300
+}
+	  
+	*/
+	
+	
+	@Path("/getProductinJSON")
+	@Produces(MediaType.APPLICATION_JSON)
+	
+	@GET
+	public Product searchProduct1(@QueryParam("pid") Integer pid) {
+		System.out.println("entered in searchProduct1 Method");
+		Product p = null;
+		if (pid != null && pid.equals(301)) {
+			p = new Product();
+			p.setPid(pid);
+			p.setPname("computer");
+			p.setPrice(300.0);
+		}
+
+		return p;
+
+	}
+
+	
+	
+	
+	/*
+	3 TEXT_PLAIN 
+	  
+	Give Request 
+	Input:
+	http://localhost:8080/Restful_Jersey_QueryParamExamples/ProductService/getProductinTEXT_PLAIN?pid=301
+	
+	
+	Out:
+	 put:keyBoard
+
+	
+	*/
+	
+	/*
+	@GET
+	@Path("/getProductinTEXT_PLAIN")
+	@Produces(MediaType.TEXT_PLAIN)
+	
+	
+	public String searchProduct1(@QueryParam("pid") Integer pid) {
+		System.out.println("entered in searchProduct1 Method");
+		Product p = null;
+		if (pid != null && pid.equals(301)) {
+			return "keyBoard";
+		}
+
+		return "Product not Found";
+
+	}
+
+	*/
+	
+	
+	
+	
+	
+	
+	
+}
